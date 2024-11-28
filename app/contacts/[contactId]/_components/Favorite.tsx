2ed264bfcfba5385b5b9e3df22fdc98d01416e72
Aurora Scharff <aurora.sofie@gmail.com>
2024-11-28 16:16:19 +0800

    update

--- app/contacts/[contactId]/_components/Favorite.tsx
+++ app/contacts/[contactId]/_components/Favorite.tsx
@@ -1,29 +1,36 @@
 'use client';
 
-import React, { useTransition } from 'react';
+import React, { useOptimistic, useTransition } from 'react';
 import { favoriteContact } from '@/data/actions/favoriteContact';
 import { cn } from '@/utils/cn';
 import type { Contact } from '@prisma/client';
 
-export default function FavoritePessimistic({ contact }: { contact: Contact }) {
-  const favorite = contact.favorite;
-  const [isPending, startTransition] = useTransition();
+export default function Favorite({ contact }: { contact: Contact }) {
+  const favoriteContactById = favoriteContact.bind(null, contact.id, contact.favorite);
+  const [optimisticFavorite, setOptimisticFavorite] = useOptimistic(contact.favorite);
+  const [, startTransition] = useTransition();
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    startTransition(async () => {
+      setOptimisticFavorite(!optimisticFavorite);
+      await favoriteContactById();
+    });
+  };
 
   return (
-    <button
-      onClick={() => {
-        startTransition(async () => {
-          await favoriteContact(contact.id, favorite);
-        });
-      }}
-      type="submit"
-      className={cn(
-        favorite || isPending ? 'text-yellow-500' : 'text-gray-dark',
-        'flex text-2xl font-normal shadow-none hover:text-yellow-400 hover:shadow-none',
-      )}
-      aria-label={favorite ? 'Remove from favorites' : 'Add to favorites'}
-    >
-      {favorite && !isPending ? '★' : '☆'}
-    </button>
+    <form action={favoriteContactById} onSubmit={onSubmit}>
+      <button
+        type="submit"
+        className={cn(
+          optimisticFavorite ? 'text-yellow-500' : 'text-gray-dark',
+          'flex text-2xl font-normal shadow-none hover:text-yellow-400 hover:shadow-none',
+        )}
+        aria-label={optimisticFavorite ? 'Remove from favorites' : 'Add to favorites'}
+      >
+        {optimisticFavorite ? '★' : '☆'}
+      </button>
+      <input type="hidden" name="favorite" value={optimisticFavorite.toString()} />
+    </form>
   );
 }
