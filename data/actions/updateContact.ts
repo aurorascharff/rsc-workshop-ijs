18caeb45deab447415697f7eaea59ac7def44aa9
Aurora Scharff <aurora.sofie@gmail.com>
2024-11-28 14:30:44 +0800

    add slow

--- data/actions/updateContact.ts
+++ data/actions/updateContact.ts
@@ -2,9 +2,12 @@
 
 import { redirect } from 'next/navigation';
 import { prisma } from '@/db';
+import { slow } from '@/utils/slow';
 import { routes } from '@/validations/routeSchema';
 
 export async function updateContact(contactId: string, formData: FormData) {
+  await slow();
+
   const data = Object.fromEntries(formData);
 
   await prisma.contact.update({
