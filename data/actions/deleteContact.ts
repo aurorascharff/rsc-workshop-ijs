e3d485ca7b0c39b074edebcc0c216fcd75f1e399
Aurora Scharff <aurora.sofie@gmail.com>
2024-11-28 14:22:12 +0800

    delete contact

---
+++ data/actions/deleteContact.ts
@@ -0,0 +1,15 @@
+'use server';
+
+import { redirect } from 'next/navigation';
+import { prisma } from '@/db';
+import { routes } from '@/validations/routeSchema';
+
+export async function deleteContact(contactId: string) {
+  await prisma.contact.delete({
+    where: {
+      id: contactId,
+    },
+  });
+
+  redirect(routes.home());
+}
