145de9ecc018621a96e8b4553076d08ee7a0c236
Aurora Scharff <aurora.sofie@gmail.com>
2024-11-28 13:47:33 +0800

    update

--- app/(intro)/client-server/page.tsx
+++ app/(intro)/client-server/page.tsx
@@ -5,7 +5,10 @@ import ClientComponent from './_components/ClientComponent';
 import ServerComponent from './_components/ServerComponent';
 
 export default function ClientServerPage() {
+  // The function will have a hidden API endpoint created and be called on the client side with "use server"
   async function mutateData() {
+    'use server';
+
     const data = await prisma.contact.findMany();
     return data[0].first as string;
   }
