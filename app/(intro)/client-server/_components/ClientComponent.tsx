6b5ebcda22cbeebb3a55653ea8d10d8f2b5e0f47
Aurora Scharff <aurora.sofie@gmail.com>
2024-11-28 13:44:13 +0800

    update

--- app/(intro)/client-server/_components/ClientComponent.tsx
+++ app/(intro)/client-server/_components/ClientComponent.tsx
@@ -3,15 +3,17 @@
 type Props = {
   children: React.ReactNode;
   content: React.ReactNode;
+  mutateData: () => Promise<string>;
 };
 
-export default function ClientComponent({ children, content }: Props) {
+export default function ClientComponent({ children, content, mutateData }: Props) {
   // Without "use client" we cant use onClick, useState, useEffect, window.location etc
   return (
     <div className="rounded border-2 border-red-500 p-4">
       <button
-        onClick={() => {
-          alert('Hello');
+        onClick={async () => {
+          const data = await mutateData();
+          alert(data);
         }}
       >
         Click me
